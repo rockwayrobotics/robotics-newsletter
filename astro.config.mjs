cd76aeebc3a05855b026d24b9a6ca06bf4cf1df0
Tnixc <tnixxc@gmail.com>
2023-12-16 21:33:44 -0500

    Update Astro config and post page layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
   markdown: {
     syntaxHighlight: "shiki",
     shikiConfig: {
-      theme: "rose-pine",
+      theme: "vitesse-dark",
       wrap: false,
     },
   },
