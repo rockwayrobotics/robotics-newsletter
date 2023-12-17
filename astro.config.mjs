63927619056caae424d3036c7504c4e480a2a20f
Tnixc <tnixxc@gmail.com>
2023-12-16 22:08:50 -0500

    Remove unused import and replace astro-imagetools component with img tag in Footer.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
-import { astroImageTools } from 'astro-imagetools';
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compress(), astroImageTools,],
+  integrations: [tailwind(), compress()],
   markdown: {
     syntaxHighlight: "shiki",
     shikiConfig: {
